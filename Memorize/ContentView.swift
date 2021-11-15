224a38e25e8f62587fbcee6b229ed01f9ddae467
wecken <wecken.rainbow@gmail.com>
2021-11-15 19:42:04 +0900

    Can shuffle arrays when theme select button is pressed.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,19 +10,19 @@ import SwiftUI
 struct ContentView: View {
   struct theme {
     var car = ["🚗", "🚘", "🚙", "🚐", "🚌", "🚚", "🚛", "🚑", "🚒", "🏎", "🚓"]
-    var animals = ["🐼", "🦢", "🐔", "🐻", "🐶", "🐱", "🐧", "🐘", "🦒", "🦍", "🐵"]
-    var nature = ["⛰", "🌞", "🌀", "🌬", "🌕", "🍀", "🌊", "🌴", "🌷", "🌍", "⚡️"]
+    var animals = ["🐼", "🦢", "🐔", "🐻", "🐶", "🐱", "🐧", "🐘", "🦒", "🦍" ]
+    var nature = ["⛰", "🌞", "🌀", "🌬", "🌕", "🍀", "🌊", "🌴", "🌷"]
   }
 //  var emojis = ["👹", "🎵", "🎤", "📛", "💻", "🌞", "⚡️", "☂️", "🍭", "🍎", "🍺", "🚗", "🍞", "🎮", "⛰", "💡", "📱", "💮" ]
   @State var selectedTheme = theme().car
-  @State var emojiCount = 5
+
     var body: some View {
 //      let emojis = theme()
       VStack{
         Text("Memorize!").font(.largeTitle)
         ScrollView {
           LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
-            ForEach(selectedTheme[0..<emojiCount], id: \.self) { emoji in
+            ForEach(selectedTheme[0..<selectedTheme.count], id: \.self) { emoji in
               CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
             }
           }
@@ -34,11 +34,12 @@ struct ContentView: View {
         themeSelections
       }
     }
+
   var themeSelections: some View {
     HStack{
-      themeSelectButton(title: "Car", image: Image(systemName: "car"), action: {selectedTheme = theme().car})
-      themeSelectButton(title: "Animals", image: Image(systemName: "pawprint.fill"), action: {selectedTheme = theme().animals})
-      themeSelectButton(title: "Nature", image: Image(systemName: "leaf"), action: {selectedTheme = theme().nature})
+      themeSelectButton(title: "Car", image: Image(systemName: "car"), action: {selectedTheme = theme().car.shuffled()})
+      themeSelectButton(title: "Animals", image: Image(systemName: "pawprint.fill"), action: {selectedTheme = theme().animals.shuffled()})
+      themeSelectButton(title: "Nature", image: Image(systemName: "leaf"), action: {selectedTheme = theme().nature.shuffled()})
     }
     .frame(maxWidth: .infinity)
   }
@@ -48,39 +49,19 @@ struct ContentView: View {
     let image: Image
     var action = {}
     var body: some View{
-      VStack {
         Button(
           action: action,
           label: {
-            image
+            VStack {
+              image
+              Text(title)
+            }
           }
         )
-        Text(title)
-      }
         .frame(minWidth: 0,  maxWidth: .infinity)
         .foregroundColor(.blue)
     }
 
-  }
-  var remove: some View {
-    Button(action: {
-      if emojiCount > 1 {
-        emojiCount -= 1
-      }
-    }, label: {
-      Image(systemName: "minus.circle")
-    })
-  }
-  var add: some View {
-    return Button(
-      action: {
-      if emojiCount < selectedTheme.count {
-        emojiCount += 1
-      }
-    },
-      label: {
-      Image(systemName: "plus.circle")
-    })
   }
 }
 
